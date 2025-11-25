91f393d1628acd95cff9fdb60247cae4c39a199c
Awoniyi-Semilore <semiloreawoniyi@gmail.com>
2025-11-25 13:01:42 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import theme from "./styles/theme";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./App";
 import { AuthProvider } from "./Hooks/useAuth.jsx";
-import { ThemeProvider } from "./context/Theme context.jsx"; // Import ThemeProvider
+import { ThemeProvider } from "./context/ThemeContext.jsx"; // Import ThemeProvider
 import ErrorBoundary from "./component/ErrorBoundary";
 import "./App.css"; 
 
